c240587c9322d882a9a3a06a5b98a4b9223a4796
Mark Preston <32184718+mgpreston@users.noreply.github.com>
2022-03-22 16:11:49 +0100

    Initial commit of portfolio files.

---
+++ Pages/Index.razor.js
@@ -0,0 +1,39 @@
+﻿export function startTyping(stringsToType) {
+    var typed = new Typed('.element', {
+        //strings: ["Mark Preston.", "A Software Engineer."],
+        strings: stringsToType,
+        typeSpeed: 120,
+        backSpeed: 60,
+        backDelay: 3000,
+        loop: true
+    });
+}
+
+export function workLoaded() {
+    //Work
+    var $container = $('.work-filter');
+    var $filter = $('#menu-filter');
+    $container.isotope({
+        filter: '*',
+        layoutMode: 'masonry',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear'
+        }
+    });
+
+    $filter.find('a').on("click", function () {
+        var selector = $(this).attr('data-filter');
+        $filter.find('a').removeClass('active');
+        $(this).addClass('active');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                animationDuration: 750,
+                easing: 'linear',
+                queue: false,
+            }
+        });
+        return false;
+    });
+}
---
+++ Shared/NavMenu.razor.js
@@ -0,0 +1,12 @@
+﻿//scroll
+export function initNavMenu() {
+    var navbar = document.querySelector('nav')
+    window.onscroll = function () {
+        // pageYOffset or scrollY
+        if (window.pageYOffset > 100) {
+            navbar.classList.add('stickyadd')
+        } else {
+            navbar.classList.remove('stickyadd')
+        }
+    }
+}
